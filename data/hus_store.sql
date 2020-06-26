d2da83095d1ff52f6721f0eb9fb60bc384898863
Huynh Tan Khoa <khoa@husol.org>
2020-06-26 20:41:52 +0700

    Update database structure

--- data/hus_store.sql
+++ data/hus_store.sql
@@ -1,8 +1,8 @@
 /*
-SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 10.3.22-MariaDB-0+deb10u1 : Database - hus_store
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.3.22-MariaDB-0+deb10u1 : Database - hus_store
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -35,61 +35,140 @@ CREATE TABLE `categories` (
 
 /*Data for the table `categories` */
 
+/*Table structure for table `comments` */
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `product_id` bigint(20) unsigned NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `status` enum('INACTIVE','ACTIVE','DELETED') CHARACTER SET utf8 NOT NULL DEFAULT 'INACTIVE',
+  `created_by` bigint(20) NOT NULL,
+  `updated_by` bigint(20) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `comments` */
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `cover` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `description` text CHARACTER SET utf8 DEFAULT NULL,
+  `status` enum('INACTIVE','ACTIVE','DELETED') CHARACTER SET utf8 NOT NULL DEFAULT 'INACTIVE',
+  `created_by` bigint(20) NOT NULL,
+  `updated_by` bigint(20) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `news` */
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `transaction_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 0,
+  `amount` decimal(19,4) NOT NULL DEFAULT 0.0000,
+  `status` enum('ACTIVE','DELETED') CHARACTER SET utf8 NOT NULL DEFAULT 'ACTIVE',
+  `created_by` bigint(20) NOT NULL,
+  `updated_by` bigint(20) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `orders` */
+
 /*Table structure for table `products` */
 
 DROP TABLE IF EXISTS `products`;
 
 CREATE TABLE `products` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `id_category` int(10) unsigned NOT NULL,
-  `model_name` varchar(64) DEFAULT NULL,
-  `name` varchar(128) DEFAULT NULL,
-  `cover` varchar(255) DEFAULT NULL,
-  `short_description` varchar(255) DEFAULT NULL,
-  `description` text DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NOT NULL,
+  `model_name` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(128) CHARACTER SET utf8 DEFAULT NULL,
+  `cover` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `short_description` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `description` text CHARACTER SET utf8 DEFAULT NULL,
   `price` decimal(19,4) NOT NULL DEFAULT 0.0000,
   `is_feature` tinyint(4) NOT NULL DEFAULT 0,
   `count_view` int(11) NOT NULL DEFAULT 0,
-  `status` enum('INACTIVE','ACTIVE','DELETED') NOT NULL DEFAULT 'INACTIVE',
+  `status` enum('INACTIVE','ACTIVE','DELETED') CHARACTER SET utf8 NOT NULL DEFAULT 'INACTIVE',
   `created_by` bigint(20) NOT NULL,
   `updated_by` bigint(20) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `idx_products_deleted_at` (`created_by`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `products` */
+
+/*Table structure for table `transactions` */
+
+DROP TABLE IF EXISTS `transactions`;
+
+CREATE TABLE `transactions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `amount` decimal(19,4) NOT NULL DEFAULT 0.0000,
+  `contact_info` text CHARACTER SET utf8 NOT NULL,
+  `deliver_place` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `note` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `status` enum('ACTIVE','DELETED') CHARACTER SET utf8 NOT NULL DEFAULT 'ACTIVE',
+  `created_by` bigint(20) NOT NULL,
+  `updated_by` bigint(20) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `idx_transactions_deleted_at` (`created_by`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `transactions` */
 
 /*Table structure for table `users` */
 
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `role` enum('ADMIN','CLIENT') DEFAULT 'CLIENT',
-  `avatar` varchar(255) DEFAULT NULL,
-  `mobile` varchar(16) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `role` enum('ADMIN','CLIENT') CHARACTER SET utf8 DEFAULT 'CLIENT',
+  `avatar` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `mobile` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `token` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `last_login` datetime DEFAULT NULL,
-  `status` enum('INACTIVE','ACTIVE','DELETED') DEFAULT 'INACTIVE',
+  `status` enum('INACTIVE','ACTIVE','DELETED') CHARACTER SET utf8 DEFAULT 'INACTIVE',
   `created_by` bigint(20) NOT NULL,
   `updated_by` bigint(20) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`full_name`,`email`,`password`,`role`,`avatar`,`mobile`,`address`,`token`,`last_login`,`status`,`created_by`,`updated_by`,`created_at`,`updated_at`) values 
-(1,'Admin Husol','enquiry@husol.org','66331e633c640a3e789db3e04cce085ffbdbdbede06058b9de1cd9d9e6bffdaa','ADMIN','','0927090050','Loc Hung - Trang Bang - Tay Ninh','','2020-04-22 15:54:45','ACTIVE',0,NULL,'2020-04-17 14:42:17','2020-04-22 15:54:45'),
-(2,'Client Husol','khoa@husol.org','ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f','CLIENT',NULL,'0929137477','Loc Hung - Trang Bang - Tay Ninh','','2020-04-20 19:31:03','ACTIVE',1,1,'2020-04-20 12:26:21','2020-04-22 16:05:56');
+insert  into `users`(`id`,`full_name`,`email`,`password`,`role`,`avatar`,`mobile`,`address`,`token`,`last_login`,`status`,`created_by`,`updated_by`,`created_at`,`updated_at`) values (1,'Admin Husol','enquiry@husol.org','ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f','ADMIN','','0927090050','Loc Hung - Trang Bang - Tay Ninh','','2020-04-22 15:54:45','ACTIVE',0,NULL,'2020-04-17 14:42:17','2020-06-26 20:39:31'),(2,'Client Husol','khoa@husol.org','ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f','CLIENT',NULL,'0929137477','Loc Hung - Trang Bang - Tay Ninh','','2020-04-20 19:31:03','ACTIVE',1,1,'2020-04-20 12:26:21','2020-04-22 16:05:56');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
