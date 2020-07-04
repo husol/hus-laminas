ad5aa77bd929dbad3103d6264313a137aff47796
Huynh Tan Khoa <khoa@husol.org>
2020-07-04 14:53:00 +0700

    Improve HusFile uploading

--- module/Core/src/Hus/HusFile.php
+++ module/Core/src/Hus/HusFile.php
@@ -16,15 +16,16 @@ class HusFile
     $this->file = $file;
   }
 
-  public function upload($destination, $id = 0)
+  public function upload($dirName, $id = 0)
   {
     try {
-      if (!is_dir(ROOT_DIR.DS.'uploads'.DS.$destination)) {
-        mkdir(ROOT_DIR.DS.'uploads'.DS.$destination, 0755, true);
+      $uploadsDir = ROOT_DIR.DS.'public'.DS.'uploads';
+      if (!is_dir($uploadsDir.DS.$dirName)) {
+        mkdir($uploadsDir.DS.$dirName, 0755, true);
       }
       $fileName = (($id > 0) ? "{$id}_" : '') . trim(preg_replace('/\s+/', '_', codau2khongdau($this->file['name'])));
-      $pathDest = DS.$destination.DS.$fileName;
-      move_uploaded_file($this->file['tmp_name'], ROOT_DIR.DS.'uploads'.$pathDest);
+      $pathDest = DS.$dirName.DS.$fileName;
+      move_uploaded_file($this->file['tmp_name'], $uploadsDir.$pathDest);
 
       return ['status' => true, 'path' => $pathDest, 'pathUrl' => str_replace('\\', '/', $pathDest)];
     }
