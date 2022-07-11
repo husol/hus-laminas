10c6680cec37bcb51b8325e15c2850d7fb6080ef
Kevin Huynh <khoaht@gmail.com>
2022-07-12 00:33:23 +0700

    Update model Dao

--- module/Application/src/Model/Contact.php
+++ module/Application/src/Model/Contact.php
@@ -20,15 +20,20 @@ class Contact extends HusDao
     return $this;
   }
 
-  public static function initDao()
+  public static function initDao($conn = null)
   {
     if (null === self::$instance) {
       $thisClass = __CLASS__;
       self::$instance = new $thisClass();
     }
+
     $dao = self::$instance;
 
-    return $dao->setConnection('main');
+    if ($conn !== null) {
+      return $dao->setConnection($conn);
+    }
+
+    return $dao->createConnection('main');;
   }
 
   /* CUSTOM DAO FUNCTIONS */
